50ec7c61928333abfe3e75444ecf8c7df79b7335
Muneer Ahmad <munirjhangir786@gmail.com>
2026-05-07 23:50:19 +0500

    Add Calculating Function [A] from Codeforces

--- 486/A - Calculating Function/solution.cpp
+++ 486/A - Calculating Function/solution.cpp
@@ -3,19 +3,11 @@ using namespace std;
 int main() {
     long long int n;
     cin>>n;
-    long long int k=0;
-    k=n/2;
-   long long  int even_sum=k*(k+1);
-   long long int odd_sum;
-   if(n%2==0)
-   {
-       odd_sum=k*k;
-   }
-   else
-   {
-   odd_sum=(k+1)*(k+1);
-   }
-   long long int ans=even_sum-odd_sum;
-cout<<ans<<endl;
+    if(n%2==0){
+        cout<<n/2<<endl;
+    }
+    else{
+        cout<<-1*((n/2)+1)<<endl;
+    }
     return 0;
 }
