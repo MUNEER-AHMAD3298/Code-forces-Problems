25cb813ba00b8792529f95969ced124cfd8f65f0
Muneer Ahmad <munirjhangir786@gmail.com>
2026-08-11 21:34:35 +0500

    Add Petya and Strings [A] from Codeforces

---
+++ 112/A - Petya and Strings/solution.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+#include <cctype>
+int main() {
+   string a,b;
+   cin>>a>>b;
+   for(int i=0;i<a.size();i++)
+   {
+    a[i]=tolower(a[i]);
+   }
+   for(int i=0;i<b.size();i++)
+   {
+    b[i]=tolower(b[i]);
+   }
+   if(a<b)
+   cout<<"-1"<<endl;
+   else if(a>b)
+   cout<<"1"<<endl;
+   else 
+   cout<<"0"<<endl;
+    return 0;
+}
