055bcc63e2263c0edc166e82ae2616e2cdae967c
Muneer Ahmad <munirjhangir786@gmail.com>
2026-08-13 15:09:08 +0500

    Add Vanya and Fence [A] from Codeforces

---
+++ 677/A - Vanya and Fence/solution.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    long long int n,h;
+    cin>>n>>h;
+    int arr[n];
+    int count=0;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+        if(arr[i]>h)
+        count+=2;
+        else
+        count++;
+         }
+         cout<<count<<endl;
+    return 0;
+}
